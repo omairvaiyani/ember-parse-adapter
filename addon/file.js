d1bf1c6650e169d6e07f568da87fc4e8286c2fc0
Omair Vaiyani <omair.vaiyani@live.co.uk>
2015-08-14 17:02:42 +0100

    Added Secure Urls to files

--- addon/file.js
+++ addon/file.js
@@ -1,3 +1,7 @@
 import Ember from 'ember';
 
-export default Ember.Object.extend();
+export default Ember.Object.extend({
+    getSecureUrl: function () {
+        return this.get('url').replace("http://", "https://s3.amazonaws.com/");
+    }
+});
